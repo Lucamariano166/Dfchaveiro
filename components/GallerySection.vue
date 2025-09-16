d8066a97c526ed81c71cfff3d459834ba1f24f08
Lucamariano166 <lucamariano.lm166@gmail.com>
2025-09-16 14:24:31 -0300

    galeria de img

--- components/GallerySection.vue
+++ components/GallerySection.vue
@@ -7,23 +7,42 @@
         v-for="(img, i) in gallery"
         :key="i"
         class="overflow-hidden rounded-lg shadow-sm hover:scale-105 transition-transform duration-200 aspect-square cursor-pointer"
-        @click="openLightbox(img)"
+        @click="openLightbox(i)"
       >
         <img
           :src="img"
           class="w-full h-full object-cover"
-          alt="Foto da galeria"
+          :alt="`Foto ${i + 1} da galeria`"
         />
       </div>
     </div>
 
     <!-- Lightbox -->
     <div
-      v-if="lightboxImg"
+      v-if="lightboxIndex !== null"
       class="fixed inset-0 bg-black bg-opacity-80 flex items-center justify-center z-50"
       @click.self="closeLightbox"
     >
-      <img :src="lightboxImg" class="max-w-full max-h-full rounded-lg shadow-lg" />
+      <button
+        @click.stop="prevImage"
+        class="absolute left-4 text-white text-3xl font-bold px-2 py-1 bg-black bg-opacity-50 rounded"
+      >
+        &#10094;
+      </button>
+
+      <img
+        :src="gallery[lightboxIndex]"
+        class="max-w-full max-h-full rounded-lg shadow-lg"
+        :alt="`Foto ${lightboxIndex + 1} da galeria`"
+      />
+
+      <button
+        @click.stop="nextImage"
+        class="absolute right-4 text-white text-3xl font-bold px-2 py-1 bg-black bg-opacity-50 rounded"
+      >
+        &#10095;
+      </button>
+
       <button
         @click="closeLightbox"
         class="absolute top-4 right-4 text-white text-2xl font-bold"
@@ -42,13 +61,25 @@ const gallery = [
   "/g.jpeg","/h.jpeg","/i.jpeg","/j.jpeg","/k.jpeg","/l.jpeg"
 ]
 
-const lightboxImg = ref(null)
+const lightboxIndex = ref(null)
 
-function openLightbox(img) {
-  lightboxImg.value = img
+function openLightbox(index) {
+  lightboxIndex.value = index
 }
 
 function closeLightbox() {
-  lightboxImg.value = null
+  lightboxIndex.value = null
+}
+
+function nextImage() {
+  if (lightboxIndex.value !== null) {
+    lightboxIndex.value = (lightboxIndex.value + 1) % gallery.length
+  }
+}
+
+function prevImage() {
+  if (lightboxIndex.value !== null) {
+    lightboxIndex.value = (lightboxIndex.value - 1 + gallery.length) % gallery.length
+  }
 }
 </script>
